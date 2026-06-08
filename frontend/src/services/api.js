583b4187435334caf7080f84a0a984c6d2d4bd77
SanthoshkumarGudi <santhosh@sigvitas.com>
2026-06-08 12:08:28 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://attorney-verification.onrender.com",
 });
 
 export default API;
